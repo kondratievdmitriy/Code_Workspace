8c079918c9b362f58750b3db736525a0ae2861a8
Дмитрий <130822046+kondratievdmitriy@users.noreply.github.com>
2025-11-15 20:03:17 +0500

    14.11
    Я не уверен, что ты сможешь понять этот безумный код (я даже не уверен что ты вообще что-то читаешь), но всё равно прикладываю его для ознакомления с задачей.

--- 2.cpp
+++ 2.cpp
@@ -1,24 +1,49 @@
+// Многочлены степени n и m заданы массивами своих коэффициентов.
+// Найти коэффициенты многочленов, являющихся суммой и произведением заданных многочленов.
+
 #include <iostream>
 using namespace std;
 
 int main() {
-    const int a = 8;
-    string s1, s2;
-    
-    getline(cin, s1);
-    getline(cin, s2);
+    const int a = 10;
+    int n, m;
+    int arr1[a] = {0};
+    int arr2[a] = {0};
 
-    bool b = true;
-    for (int i = 0; i < a; i++) {
-        if (s1[i] != s2[a - 1 - i]) {
-            b = false;
-            break;
+    cin >> n;
+    for (int i = n-1; i >= 0; i--) {
+        cout << "coefficient x^" << i << ": ";
+        cin >> arr1[i];
+    }
+    
+    cin >> m;
+    for (int i = m-1; i >= 0; i--) {
+        cout << "coefficient x^" << i << ": ";
+        cin >> arr2[i];
+    }
+    
+    int s[a] = {0};
+    for (int i = 0; i < max(n, m); i++) {
+        s[i] = arr1[i] + arr2[i];
+    }
+
+    int p[2 * a] = {0};
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            p[i + j] += arr1[i] * arr2[j];
         }
     }
-    if (b)
-        cout << "true" << endl;
-    else
-        cout << "false" << endl;
+
+    cout << "sum of polynomials: " << endl;
+    for (int i = max(n, m) - 1; i >= 0; i--) {
+        cout << s[i] << " ";
+    }
+    cout << endl;
+
+    cout << "product of polynomials: " << endl;
+    for (int i = n + m - 2; i >= 0; i--) {
+        cout << p[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
-
