3d7655c01f56d2542ebbcad0ac9cd05902c6cf26
Дмитрий <130822046+kondratievdmitriy@users.noreply.github.com>
2025-11-15 16:18:39 +0500

    14.11
    Снизу в комментариях написал, как можно изменить код, если ничего в голову не придет.

--- 1.cpp
+++ 1.cpp
@@ -1,33 +1,25 @@
+// Задана строка, в которой слова разделены одним или несколькими пробелами.
+// Выполнить сжатие строки, оставив между словами только по одному пробелу. Сжатие выполнить на том же месте.
+
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main() {
-    const char* C[] = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
-    const char* X[] = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
-    const char* I[] = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
-    string a, res;
-    cin >> a;
-
-    // Если что, то это проверка на пустую строчку
-    if (a.empty()) {
-        cout << "" << endl;
-        return 0;
+    string s;
+    int a = 0;
+    getline(cin, s);
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == ' ') {
+            a++;
+            if (a > 1) {
+                s = s.substr(0, i) + s.substr(i+1, s.length());
+                i--;
+            }
+        } else {
+            a = 0;
+        }
     }
-
-    int num = stoi(a);
-    int thousands = num / 1000;
-    int hundreds = (num % 1000) / 100;
-    int tens = (num % 100) / 10;
-    int units = num % 10;
-
-    if (thousands > 0) {
-        res += string(thousands, 'M');
-    }
-    res += C[hundreds];
-    res += X[tens];
-    res += I[units];
-    cout << res << endl;
-    return 0;
+    cout << s << endl;
 }
 
+// Можно вместо строчки с substr использовать s.erase(i, 1);
