b8136e4a4419c42c3c0722ab343047ff1ca473a5
Дмитрий <130822046+kondratievdmitriy@users.noreply.github.com>
2025-11-22 18:58:43 +0500

    21.11

--- 2.cpp
+++ 2.cpp
@@ -1,49 +1,24 @@
-// Многочлены степени n и m заданы массивами своих коэффициентов.
-// Найти коэффициенты многочленов, являющихся суммой и произведением заданных многочленов.
+// В заданной вещественной матрице вычислить количество нулевых строк.
 
 #include <iostream>
 using namespace std;
 
 int main() {
-    const int a = 10;
-    int n, m;
-    int arr1[a] = {0};
-    int arr2[a] = {0};
-
-    cin >> n;
-    for (int i = n-1; i >= 0; i--) {
-        cout << "coefficient x^" << i << ": ";
-        cin >> arr1[i];
-    }
-    
-    cin >> m;
-    for (int i = m-1; i >= 0; i--) {
-        cout << "coefficient x^" << i << ": ";
-        cin >> arr2[i];
-    }
-    
-    int s[a] = {0};
-    for (int i = 0; i < max(n, m); i++) {
-        s[i] = arr1[i] + arr2[i];
-    }
-
-    int p[2 * a] = {0};
+    int n, m, res = 0;
+    cin >> n >> m;
+    int **arr = new int *[n];
     for (int i = 0; i < n; i++) {
+        arr[i] = new int [m];
+        int a = 0;
         for (int j = 0; j < m; j++) {
-            p[i + j] += arr1[i] * arr2[j];
+            cin >> arr[i][j];
+            if (arr[i][j] == 0) {
+                a++;
+            }
+        }
+        if (a == m) {
+            res++;
         }
     }
-
-    cout << "sum of polynomials: " << endl;
-    for (int i = max(n, m) - 1; i >= 0; i--) {
-        cout << s[i] << " ";
-    }
-    cout << endl;
-
-    cout << "product of polynomials: " << endl;
-    for (int i = n + m - 2; i >= 0; i--) {
-        cout << p[i] << " ";
-    }
-    cout << endl;
-    return 0;
+    cout << res << endl;
 }
